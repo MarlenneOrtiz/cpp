175fb96f86c257121272744d4ff1d524e7b9f019
MarlenneOrtiz <106360534+MarlenneOrtiz@users.noreply.github.com>
2022-07-12 17:55:06 -0600

    Add files via upload

---
+++ Ejercicio46 Punto de venta-archivosH(tarea)/encabezados.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+
+using namespace std; 
+
+void menu();
+
+void productos ( int opcion ) ;
+
+void agregarProducto( string descripcion,int cantidad, double precio) ;
+
+void imprimirFactura() ; 
---
+++ Ejercicio46 Punto de venta-archivosH(tarea)/main6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include "encabezados.h"
+
+using namespace std; 
+
+
+
+int main(int argc, char const *argv[])
+{
+   menu();
+   
+   return 0;
+}
+
+// Realizado por Marlenne Alexa Ortiz
+
+
+
+
+
+
+
+
